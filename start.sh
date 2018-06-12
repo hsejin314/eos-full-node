4937f3243f3a90020ecd9ba77e686a6491de28d2
acroeos <40120573+acroeos@users.noreply.github.com>
2018-06-12 14:51:28 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ################################################################################
-#																			  ##
-# For EOS main-net 															  ##
-#																			  ##
-# https://github.com/acroeos/												  ##
-#																			  ##
+#																		
+# For EOS main-net 															  
+#																			 
+# https://github.com/acroeos/eos-full-node										
+#																			  
 ################################################################################
 
-NODEOS=/home/user/eos_source_DIR/build/programs/nodeos/nodeos
-DATADIR=/home/user/script_DIR
+NODEOS=/opt/eos/build/programs/nodeos/nodeos
+DATADIR=/opt/eos-full-node/
 $DATADIR/stop.sh
 $NODEOS --data-dir $DATADIR --config-dir $DATADIR "$@" > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
