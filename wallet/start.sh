74f638ab457880623312af53fa42234914102324
acroeos <40120573+acroeos@users.noreply.github.com>
2018-06-12 14:53:56 +0900

    Update start.sh

--- wallet/start.sh
+++ wallet/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATADIR=/home/user/data-dir/wallet
+DATADIR=/opt/eos-full-node/wallet
 $DATADIR/stop.sh
-/home/user/eos-dir/build/programs/keosd/keosd --data-dir $DATADIR --http-server-address 127.0.0.1:7777 "$@" > /home/sejin/Ghostbusters/wallet/stdout.txt 2> /home/sejin/Ghostbusters/wallet/stderr.txt  & echo $! > $DATADIR/wallet.pid
+/opt/eos/build/programs/keosd/keosd --data-dir $DATADIR --http-server-address 127.0.0.1:7777 "$@" > /opt/eos-full-node/wallet/stdout.txt 2> /opt/eos-full-node/wallet/stderr.txt  & echo $! > $DATADIR/wallet.pid
 echo "Wallet started"
