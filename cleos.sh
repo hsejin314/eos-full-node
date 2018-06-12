64a68114f8047ca0dd9dccd4799bbb54b0eb9f44
acroeos <40120573+acroeos@users.noreply.github.com>
2018-06-12 14:50:09 +0900

    Update cleos.sh

--- cleos.sh
+++ cleos.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 ################################################################################
-#																			  ##
-# For EOS main-net 															  ##
-#																			  ##
-# https://github.com/acroeos/												  ##
-#																			  ##
+#															                				  
+# For EOS main-net 															  
+#																			  
+# https://github.com/acroeos/eos-full-node												  
+#																			  
 ################################################################################
 
-CLEOS=/home/user/eos_source_DIR/build/programs/cleos/cleos
+## dir to eos-source->client
+CLEOS=/opt/eos/build/programs/cleos/cleos
 $CLEOS -u http://127.0.0.1:8881 --wallet-url http://127.0.0.1:55551 "$@"
 #$CLEOS -u <API adress> --wallet-url <wallet url> "$@"
 
